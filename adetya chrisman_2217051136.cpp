551774609c1d2899c6793067168ad65e9f036f4a
adetyachrisman <113002438+adetyachrisman@users.noreply.github.com>
2022-09-22 20:00:11 +0700

    Add files via upload

---
+++ adetya chrisman_2217051136.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	char huruf = '1';
+	switch(huruf){
+	case '1' :
+		cout<<"satu\n";
+		break;
+	case '2' :
+		cout<<"dua\n";
+		break;
+	case '3' :
+		cout<<"tiga\n";
+		break;
+	defaut :
+		cout<<"bilangan lebih besar dari 3\N";
+	}
+}
